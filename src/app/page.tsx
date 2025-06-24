ac9699da28bad84276f1696ec9b30f0aa07635b5
Onur AKÇAY <onurakcay.tr@gmail.com>
2025-06-25 01:19:06 +0300

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -70,7 +70,7 @@ export default function Home() {
     <>
       <main className="py-8">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold mb-8">Ürünlerimiz</h1>
+          <h1 className="text-3xl font-bold mb-8">Ürünlerimizx</h1>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
             {products.map((product) => (
               <Card

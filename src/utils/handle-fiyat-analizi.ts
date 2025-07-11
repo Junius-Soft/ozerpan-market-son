eafad0d8c98cde0fe490cc1d0ab4c4663f8ebd20
Onur AKÇAY <onurakcay.tr@gmail.com>
2025-07-11 13:41:13 +0300

    test

--- src/utils/handle-fiyat-analizi.ts
+++ src/utils/handle-fiyat-analizi.ts
@@ -28,7 +28,7 @@ export async function handleFiyatAnaliziPDF({
   const fakePosition = {
     id: "-",
     pozNo: typeId || "-",
-    unit: "adet",
+    unit: "adett",
     quantity: formikValues.quantity || 1,
     unitPrice: formikValues.unitPrice || 0,
     selectedProducts: formikValues.selectedProducts || {
